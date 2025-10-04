b63e182b480b5b9ac86a7265d68749379f9c7bb7
João Souza Bispo <js_bispo1@MacBook-Air-de-Joao.local>
2025-10-04 01:23:19 -0400

    back initial state

--- script.js
+++ script.js
@@ -2,20 +2,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import { chromium } from "playwright";
 
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const app = express();
 const port = 3333;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
 	res.render("home.ejs");
